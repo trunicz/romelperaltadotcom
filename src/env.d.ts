eb725c294ca514e1d629da832ddcee2b7c91bb6c
Trunicz <romelelicg_19@alu.uabcs.mx>
2024-08-08 17:30:24 -0700

    init - add tailwind- and create MainLayout

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,13 @@
 /// <reference types="astro/client" />
-type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
+type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+type ENV = {
+  // replace `MY_KV` with your KV namespace
+  MY_KV: KVNamespace;
+};
+
+// use a default runtime configuration (advanced mode).
+type Runtime = import("@astrojs/cloudflare").Runtime<ENV>;
 declare namespace App {
-	interface Locals extends Runtime {}
+  interface Locals extends Runtime {}
 }
