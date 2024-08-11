73093735fa55347ea3e92266b0d3f7c3b5d6cb10
Trunicz <romelelicg_19@alu.uabcs.mx>
2024-08-11 13:04:24 -0700

    Complete Init commit

---
+++ src/components/home/Header.tsx
@@ -0,0 +1,15 @@
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header>
+      <div className="container my-8">
+        <div className="flex justify-center">
+          <img className="w-12" src="/RomelPeraltaLogo.png" alt="" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    './pages/**/*.{astro,ts,tsx}',
+    './components/**/*.{astro,ts,tsx}',
+    './app/**/*.{astro,tsx}',
+    './src/**/*.{astro,tsx}',
+    './node_modules/flowbite/**/*.js'
+  ],
   prefix: "",
   theme: {
     container: {
@@ -47,26 +53,11 @@ module.exports = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"]
+      }
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require('flowbite/plugin')],
 }
